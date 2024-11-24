bf4b6f2b3c4c4bdcb5207002366993dfaffd220f
dsddr02 <fldz5555a@outlook.com>
2024-11-24 13:50:20 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
-
+rm -rf feeds/packages/net/v2ray-geodata
 # Default IP
-sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.110.247/g' package/base-files/files/bin/config_generate
 
 # Remove packages
 rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-passwall2
 
+rm -rf feeds/luci/applications/luci-app-netdata
+rm -rf feeds/luci/themes/luci-theme-argon
 # Add packages
-git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
+git clone https://github.com/kenzok8/small-package package/small-package
+# git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
+rm -rf package/small-package/luci-app-openvpn-server
+rm -rf package/small-package/openvpn-easy-rsa-whisky
+rm -rf package/small-package/luci-app-wrtbwmon
+rm -rf package/small-package/wrtbwmon
+rm -rf package/small-package/luci-app-koolproxyR
+rm -rf package/small-package/luci-app-godproxy
+rm -rf package/small-package/luci-app-argon*
+rm -rf package/small-package/luci-theme-argon*
+rm -rf package/small-package/luci-app-amlogic
+rm -rf package/small-package/luci-app-unblockneteasemusic
+rm -rf package/small-package/upx-static
+rm -rf package/small-package/upx
+rm -rf package/small-package/firewall*
+rm -rf package/small-package/opkg
+rm -rf package/feeds/packages/aliyundrive-webdav
+rm -rf feeds/packages/multimedia/aliyundrive-webdav
+rm -rf package/feeds/packages/perl-xml-parser
+rm -rf package/feeds/packages/xfsprogs
