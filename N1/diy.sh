df0b18731d536e591f193c193acf9f4511025396
dsddr02 <fldz5555a@outlook.com>
2024-12-31 12:18:50 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 # rm -rf feeds/packages/net/v2ray-geodata
 # Git稀疏克隆，只克隆指定目录到本地
-function git_sparse_clone() {
-  branch="$1" repourl="$2" && shift 2
-  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
-  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
-  cd $repodir && git sparse-checkout set $@
-  mv -f $@ ../package
-  cd .. && rm -rf $repodir
-}
+
 
 # Default IP
 sed -i 's/192.168.1.1/192.168.110.247/g' package/base-files/files/bin/config_generate
@@ -25,35 +18,7 @@ git_sparse_clone main https://github.com/xiaorouji/openwrt-passwall2 luci-app-pa
 #git_sparse_clone main https://github.com/kenzok8/small-package luci-app-store
 #git_sparse_clone main https://github.com/morytyann/OpenWrt-mihomo luci-app-mihomo mihomo
 git clone https://github.com/kenzok8/small-package package/small-package 
-git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages 
+# git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages 
 # sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 # sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-rm -rf package/small-package/luci-app-openvpn-server
-rm -rf package/small-package/openvpn-easy-rsa-whisky
-rm -rf package/small-package/luci-app-wrtbwmon
-rm -rf package/small-package/wrtbwmon
-rm -rf package/small-package/luci-app-koolproxy
-rm -rf package/small-package/luci-app-godproxy
-rm -rf package/small-package/luci-app-argon*
-rm -rf package/small-package/luci-theme-argon*
-rm -rf package/small-package/luci-app-amlogic
-rm -rf package/small-package/luci-app-unblockneteasemusic
-rm -rf package/small-package/upx-static
-rm -rf package/small-package/upx
-rm -rf package/small-package/firewall*
-rm -rf package/small-package/opkg
-rm -rf package/small-package/base-files
-rm -rf package/small-package/luci-app-bandwidthd
-rm -rf package/small-package/luci-app-bypass
-rm -rf package/small-package/luci-app-dogcom
-rm -rf package/small-package/luci-app-gowebdav
-rm -rf package/small-package/luci-app-nginx-pingos
-rm -rf package/small-package/luci-app-ssr-plus
-rm -rf package/small-package/luci-app-onliner
-rm -rf package/small-package/natflow
-rm -rf package/small-package/luci-app-passwall2
 
-rm -rf package/feeds/packages/aliyundrive-webdav
-rm -rf feeds/packages/multimedia/aliyundrive-webdav
-rm -rf package/feeds/packages/perl-xml-parser
-rm -rf package/feeds/packages/xfsprogs
