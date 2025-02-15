50fd7f60790689fb0db824b937f4ecb75afe5c75
dsddr02 <fldz5555a@outlook.com>
2025-02-15 23:49:07 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -16,7 +16,7 @@ rm -rf feeds/luci/applications/luci-app-passwall2
 #git_sparse_clone main https://github.com/kenzok8/small-package luci-lib-taskd
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
 #git_sparse_clone main https://github.com/xiaorouji/openwrt-passwall luci-app-passwall
-#git_sparse_clone main https://github.com/xiaorouji/openwrt-passwall2 luci-app-passwall2
+git clone main https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 #git_sparse_clone main https://github.com/kenzok8/small-package luci-app-store
 #git_sparse_clone main https://github.com/morytyann/OpenWrt-mihomo luci-app-mihomo mihomo
 #git clone https://github.com/kenzok8/small-package package/small-package 
