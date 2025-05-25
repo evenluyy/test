8691cc6a59d34da75ea273806ab61356961bdf09
evenluyy <evenluyy@outlook.com>
2025-05-25 13:50:01 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -13,7 +13,7 @@ rm -rf feeds/luci/applications/luci-app-passwall2
 rm -rf feeds/packages/net/cdnspeedtest
 # rm -rf feeds/packages/gst1-plugins-base
 # cf测速换ip两个插件
-git clone https://github.com/immortalwrt-collections/openwrt-cdnspeedtest package/cdnspeedtest
+git clone https://github.com/dsddr02/cdnspeedtest package/cdnspeedtest
 git clone https://github.com/dsddr02/peed11 package/luci-app-cloudflarespeedtest
 #git_sparse_clone main https://github.com/kenzok8/small-package luci-lib-taskd
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
