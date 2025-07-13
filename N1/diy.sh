7c5c7e793a11606772f7d7ae3066f5a6d610299c
evenluyy <evenluyy@outlook.com>
2025-07-13 22:08:54 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -57,3 +57,5 @@ rm -rf package/feeds/packages/perl-xml-parser
 rm -rf package/feeds/packages/xfsprogs
 rm -rf package/feeds/packages/alist
 rm -rf feeds/packages/net/alist
+rm -rf feeds/luci/applications/luci-app-openlist
+rm -rf package/small-package/luci-app-openlist
