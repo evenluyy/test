ab3ded45ad925bd1c900241d6abde361ed9f9e69
evenluyy <evenluyy@outlook.com>
2025-09-19 18:54:07 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -11,7 +11,7 @@ rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/luci/applications/luci-app-alist
 rm -rf feeds/luci/applications/luci-app-passwall2
 rm -rf feeds/packages/net/cdnspeedtest
-rm -rf feeds/packages/alist
+# rm -rf feeds/packages/gst1-plugins-base
 # cf测速换ip两个插件
 git clone https://github.com/dsddr02/cdnspeedtest package/cdnspeedtest
 git clone https://github.com/dsddr02/peed11 package/luci-app-cloudflarespeedtest
@@ -23,22 +23,42 @@ git clone https://github.com/sbwml/luci-app-openlist2 package/luci-app-openlist2
 #git_sparse_clone main https://github.com/morytyann/OpenWrt-mihomo luci-app-mihomo mihomo
 git clone https://github.com/kenzok8/small-package package/small-package 
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages 
-
+# sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+# sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+rm -rf package/small-package/luci-app-openvpn-server
+rm -rf package/small-package/openvpn-easy-rsa-whisky
+rm -rf package/small-package/luci-app-wrtbwmon
+rm -rf package/small-package/wrtbwmon
+rm -rf package/small-package/luci-app-koolproxy
+rm -rf package/small-package/luci-app-godproxy
 rm -rf package/small-package/luci-app-passwall2
 #rm -rf package/small-package/luci-theme-argon*
 rm -rf package/small-package/luci-app-amlogic
-# rm -rf package/small-package/{natflow,luci-app-nginx-pingos,luci-app-gowebdav,luci-app-dogcom,luci-app-bandwidthd,wireless-regdb}
-rm -rf package/small-package/luci-app-cloudflarespeedtest
-rm -rf package/small-package/natflow
-rm -rf package/small-package/luci-app-nginx-pingos
+rm -rf package/small-package/cdnspeedtest
+rm -rf package/small-package/luci-app-unblockneteasemusic
+rm -rf package/small-package/upx-static
+rm -rf package/small-package/upx
+rm -rf package/small-package/firewall*
+rm -rf package/small-package/opkg
+rm -rf package/small-package/base-files
 rm -rf package/small-package/luci-app-bandwidthd
-rm -rf package/small-package/luci-app-gowebdav
-
+rm -rf package/small-package/luci-app-bypass
 rm -rf package/small-package/luci-app-dogcom
-# rm -rf package/feeds/packages/alist
-# rm -rf feeds/packages/net/alist
+rm -rf package/small-package/luci-app-gowebdav
+rm -rf package/small-package/luci-app-nginx-pingos
+rm -rf package/small-package/luci-app-ssr-plus
+rm -rf package/small-package/luci-app-onliner
+rm -rf package/small-package/natflow
+rm -rf package/small-package/luci-app-cloudflarespeedtest
+#rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
+rm -rf package/feeds/packages/aliyundrive-webdav
+rm -rf feeds/packages/multimedia/aliyundrive-webdav
+rm -rf package/feeds/packages/perl-xml-parser
+rm -rf package/feeds/packages/xfsprogs
+#rm -rf package/feeds/packages/alist
+#rm -rf feeds/packages/net/alist
 rm -rf feeds/luci/applications/luci-app-openlist
 rm -rf package/small-package/luci-app-openlist
 rm -rf feeds/luci/applications/luci-app-openlist2
 rm -rf package/small-package/luci-app-openlist2
-
+rm -rf package/small-package/alist
